d9decc96293335c6a6c7a195577bec69bf5bbf93
Raghu <khajuriaraghu0@gmail.com>
2025-05-22 22:47:58 +0530

    fix error

--- controllers/adminCtrl.js
+++ controllers/adminCtrl.js
@@ -44,30 +44,31 @@ const getAllDoctorsController = async (req, res) => {
 
 const changeAccountStatusController = async (req, res) => {
     try {
-        const { doctorId, status } = req.body;
+        const { doctorId, status } = req.body
         const doctor = await doctorModel.findByIdAndUpdate(doctorId, { status });
         const user = await userModel.findOne({ _id: doctor.userId });
-        const notifcation = user.notifcation;
-        notifcation.push({
-            type: "doctor-account-request-updated",
-            message: `Your Doctor Account Request Has ${status} `,
-            onClickPath: "/notification",
-        });
-        user.isDoctor = status === "approved" ? true : false;
+        const notification = user.notification
+        notification.push({
+            type: 'doctor-account-request-updated',
+            message: `Your Doctor Account Request Has ${status}`,
+            onClickPath: '/notification'
+        })
+        user.isDoctor = status === 'approved' ? true : false
+
         await user.save();
         res.status(201).send({
             success: true,
-            message: "Account Status Updated",
-            data: doctor,
-        });
+            message: 'Account Status Updated',
+            data: doctor
+        })
     } catch (error) {
         console.log(error);
         res.status(500).send({
             success: false,
-            message: "Eror in Account Status",
-            error,
-        });
+            message: 'error in account status',
+            error
+        })
     }
-};
+}
 
 module.exports = { getAllUsersController, getAllDoctorsController, changeAccountStatusController }
